3cc10c0629e27b8db038aa7a4aa912de70985d93
Angelika Brown <angelikpbrown@gmail.com>
2025-02-28 14:02:23 -0500

    passed grosspay tests

--- src/main/java/rocks/zipcode/PayDay.java
+++ src/main/java/rocks/zipcode/PayDay.java
@@ -58,11 +58,13 @@ public class PayDay {
 
     // each of these will be USED inside of pay() ^^^^^
     public double grossPay(double rate, double hours) {
-        return -1.0;
+        double gp = rate * hours;
+        return gp;
     }
     public double deductTax(double gross, double taxRate) {
         return -1.0;
     }
+        double tax =
     public double netPay(double gross, double deduction) {
         return -1.0;
     }
